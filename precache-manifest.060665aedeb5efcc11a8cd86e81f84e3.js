096fbd44aab68055e9589dd850e931f77ee9182c
matas <matasspeicys@gmail.com>
2019-12-07 18:10:31 +0200

    Updates

---
+++ precache-manifest.060665aedeb5efcc11a8cd86e81f84e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857fdf40eca849b836b0e258e8c097f6",
+    "url": "/skydiving-ode/index.html"
+  },
+  {
+    "revision": "4729a9ae7621499e0718",
+    "url": "/skydiving-ode/static/css/main.b414bf1a.chunk.css"
+  },
+  {
+    "revision": "5f0bcbfaccf0ed6074c8",
+    "url": "/skydiving-ode/static/js/2.bf44add6.chunk.js"
+  },
+  {
+    "revision": "7cc73b32417e46380ffdfbd88895e432",
+    "url": "/skydiving-ode/static/js/2.bf44add6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4729a9ae7621499e0718",
+    "url": "/skydiving-ode/static/js/main.ba104473.chunk.js"
+  },
+  {
+    "revision": "57dbec5b77b97a40666d",
+    "url": "/skydiving-ode/static/js/runtime-main.c8484c37.js"
+  }
+]);
